616e7fca0767e62dacec8f40c14ec6d4d3857be0
VonJoke <simplyme.vs@gmail.com>
2023-09-06 17:54:47 +0200

    1 file modified

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ let name = 'Google';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `
   <h1>This is 
-    <a target="_blank" href="https://google.com"> ${name} </a>
+    <a id="googleLink" target="_blank" href="https://google.com"> ${name} </a>
   </h1>
 `;
+
+const googleLink = document.querySelector('h1');
+googleLink.addEventListener('click', show);
+
+function show() {
+  alert('No');
+}
